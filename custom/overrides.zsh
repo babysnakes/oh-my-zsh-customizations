8da3b55561c83c57d8bf81af70221474aeb81004
Haim Ashkenazi <haim@babysnakes.org>
2011-12-28 21:56:38 +0200

    Removed all aliases created by default in oh-my-zsh.

--- custom/overrides.zsh
+++ custom/overrides.zsh
@@ -3,5 +3,13 @@
 # EsC+. should be the last word! not ..
 bindkey '\e.' insert-last-word
 
-# correctons are annoying
+# correctons are annoying. Undo all corrections.
 unsetopt correct_all
+unalias man
+unalias mv
+unalias mysql
+unalias mkdir
+unalias gist
+unalias heroku
+unalias ebuild
+unalias hpodder
