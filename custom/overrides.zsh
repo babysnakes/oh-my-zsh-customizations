e9f9b4f464427560e9462a7333a53ae0bc86774b
Haim Ashkenazi <haim@babysnakes.org>
2013-05-16 23:51:55 +0300

    After an upgrade these overrides produced errors.

--- custom/overrides.zsh
+++ custom/overrides.zsh
@@ -5,11 +5,3 @@ bindkey '\e.' insert-last-word
 
 # correctons are annoying. Undo all corrections.
 unsetopt correct_all
-unalias man
-unalias mv
-unalias mysql
-unalias mkdir
-unalias gist
-unalias heroku
-unalias ebuild
-unalias hpodder
