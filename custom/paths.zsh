be49fba1caf4211c08d16a88d9291957367e1c17
Haim Ashkenazi <haim@babysnakes.org>
2013-05-29 09:45:08 +0300

    Added function to remove path element.

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -5,7 +5,24 @@ function add-to-path {
     return 1
   else
     PATH_TO_ADD=$(echo "$1"(:A))
-    eval "export PATH=${PATH_TO_ADD}:${PATH}"
+    export PATH=${PATH_TO_ADD}:${PATH}
+    rehash
+  fi
+}
+
+# remove from path (idea borrowed from nvm).
+# This won't work on last element in $PATH because for safety reasons
+# I'm matching a ":" after the path element.
+function remove-from-path {
+  if [[ -z $1 ]]; then
+    echo "Usage: remove-from-path DIR"
+    return 1
+  else
+    PATH_TO_REMOVE=$(echo "$1"(:A))
+    if [[ $PATH == *$PATH_TO_REMOVE:* ]]; then
+      export PATH=${PATH%$PATH_TO_REMOVE*}${PATH#*$PATH_TO_REMOVE:}
+      rehash
+    fi
   fi
 }
 
